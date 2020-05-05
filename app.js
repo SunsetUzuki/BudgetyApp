6005aa6eacdf12ff259968cca328effe32c2b570
Shuleu Illia <Illia.Shuleu@itechart-group.com>
2020-05-05 14:01:14 +0300

    Added DOMstrings var

--- app.js
+++ app.js
@@ -10,7 +10,26 @@ var budgetController = (function() {
 //UI CONTROLLER
 var UIController = (function() {
 
-    
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+
+    }
+    return {
+        getInput: function() {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, //will be either inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value,
+            };
+        },
+
+        getDOMstrings: function(){
+            return DOMstrings;
+        }
+    };
 
 })();
 
@@ -19,24 +38,29 @@ var UIController = (function() {
 //GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl)  {
 
+    var DOM = UICtrl.getDOMstrings();
+
     var ctrlAddItem = function() {
 
         //1. get the field input data
+        var input = UICtrl.getInput();
+        console.log(input);
+
+
         //2. add the item to budget controller
         //3. add the item to the UI
         //4. calculate the budget
         //5. display the budget on the UI
 
-        console.log('It works')
     }
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function(event) {
 
         if(event.keyCode === 13 || event.which === 13) {
             ctrlAddItem();
-        }
+        };
 
     });
 
