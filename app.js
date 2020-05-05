37f6b3730389de6f9c37a0d6a9406eb2da1f052f
Shuleu Illia <Illia.Shuleu@itechart-group.com>
2020-05-05 13:37:14 +0300

    added eventlisteners

--- app.js
+++ app.js
@@ -1,34 +1,43 @@
+//BUDGET CONTROLLER
 var budgetController = (function() {
-    var x = 23;
-
-    var add = function(a) {
-        return x + a;
-    }
-
-    return {
-        publicTest: function(b) {
-            return add(b);
-        }
-    }
-
-})();
-
-
-var UIController = (function() {
 
     //some code
 
 })();
 
 
+
+//UI CONTROLLER
+var UIController = (function() {
+
+    
+
+})();
+
+
+
+//GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl)  {
 
-    var z = budgetCtrl.publicTest(5);
+    var ctrlAddItem = function() {
 
-    return {
-        anotherPublic: function() {
-            console.log(z);
-        }
+        //1. get the field input data
+        //2. add the item to budget controller
+        //3. add the item to the UI
+        //4. calculate the budget
+        //5. display the budget on the UI
+
+        console.log('It works')
     }
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event) {
+
+        if(event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+
+    });
 
 })(budgetController, UIController);
